5f0cefe12e80406f1e72df3fbd809eae328d773d
Abner Yohan Sato <102881830+SatoYohan@users.noreply.github.com>
2025-06-25 00:48:51 -0300

    feat: add routes new blades

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/game1', function () {
+    return view('games/game1');
+});
+
+Route::get('/game2', function () {
+    return view('games/game2');
+});
